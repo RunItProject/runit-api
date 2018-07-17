744d0d72f8c415f055d36587afc0b973f0594f97
Mortimer <mortyh@gmail.com>
2018-07-17 16:24:28 +0200

    Copy production appsettings into build

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 
 rsync -r --delete $TRAVIS_BUILD_DIR/publish/ travis@rancor.mortimer.nu:/var/travis/runit-api/$TRAVIS_BUILD_ID
 ssh -t travis@rancor.mortimer.nu "rm /var/travis/runit-api/current && ln -s /var/travis/runit-api/$TRAVIS_BUILD_ID /var/travis/runit-api/current"
+ssh -t travis@rancor.mortimer.nu "cp /var/aspnetcore/appsettings.Production.json /var/travis/runit-api/current/"
 ssh -t travis@rancor.mortimer.nu "sudo /bin/systemctl restart kestrel-runit-api.service"
